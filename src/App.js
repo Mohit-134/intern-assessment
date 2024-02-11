5f652d9a646be669b24ffd63ca94c7b62ec872a1
Mohit-134 <109890852+Mohit-134@users.noreply.github.com>
2024-02-11 11:40:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((data) => setUserData(data.results[0]))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black flex justify-center items-center h-screen">
+      <div className="bg-white p-8 w-[30%] h-[30%]">
+        {userData && (
+          <div>
+            <div className=" border-black border-[6px] p-8  flex justify-stretch space-x-7">
+              <div className=" border-black border-[6px]">
+                <img width={120}  src={userData.picture.large} alt="User" />
+              </div>
+              <div className="space-y-4">
+                <p>
+                   {userData.name.first} {userData.name.last}
+                </p>
+                <p> {userData.gender}</p>
+                <p> {userData.phone}</p>
+
+                
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
